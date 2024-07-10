7dffda5a2c0597c0c04e212af62c4b206b505fe9
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-07-10 20:44:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+ Node* sortedMerge(Node* head1, Node* head2) {
+    if (head1 == nullptr) return head2;
+    if (head2 == nullptr) return head1;
+    
+    Node* result = nullptr;
+    
+    if (head1->data <= head2->data) {
+        result = head1;
+        head1 = head1->next;
+    } else {
+        result = head2;
+        head2 = head2->next;
+    }
+    
+    Node* current = result;
+    
+    while (head1 != nullptr && head2 != nullptr) {
+        if (head1->data <= head2->data) {
+            current->next = head1;
+            head1 = head1->next;
+        } else {
+            current->next = head2;
+            head2 = head2->next;
+        }
+        current = current->next;
+    }
+    
+    if (head1 != nullptr) {
+        current->next = head1;
+    } else {
+        current->next = head2;
+    }
+    
+    return result;
+}
+ 
