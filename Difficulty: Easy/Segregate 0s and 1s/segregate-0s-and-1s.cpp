4d3770fe9e1d2fa3205af8d18448767d7f0dccf6
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-07-15 21:33:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solution {
   public:
     void segregate0and1(vector<int> &arr) {
-        sort(arr.begin(),arr.end());
+        sort(arr.begin(), arr.end());
     }
 };
 
