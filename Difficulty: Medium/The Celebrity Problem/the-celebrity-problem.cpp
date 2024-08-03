6d03f2d93700e465c3db22dfa7e8e5d3e7cd370d
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-08-03 18:29:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,89 +1,54 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-#include <vector>
-#include <stack>
 
-class Solution 
-{
-    private:
-    bool knows(std::vector<std::vector<int>>& M, int a, int b) {
-        return M[a][b] == 1;
-    }
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M) {
+        int start=0, n = M.size(); int end=n-1;
     
-    public:
-    int celebrity(std::vector<std::vector<int>>& M, int n) 
-    {
-        std::stack<int> s;
-        for (int i = 0; i < n; i++) {
-            s.push(i);
-        }
-        
-        // Find the potential celebrity
-        while (s.size() > 1) {
-            int a = s.top();
-            s.pop();
-            int b = s.top();
-            s.pop();
+        while(start < end){
             
-            if (knows(M, a, b)) {
-                s.push(b);
-            } else {
-                s.push(a);
+            if(M[start][end]==1) start++;
+            
+            else end--;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(i!=start){
+                 if(M[i][start]==0 || M[start][i]==1)
+                 return -1;
             }
         }
         
-        int ans = s.top();
-        
-        // Check if the potential celebrity is a real celebrity
-        int zeroCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (M[ans][i] == 0) {
-                zeroCount++;
-            }
-        }
-        if (zeroCount != n) return -1;
-        
-        int oneCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (M[i][ans] == 1) {
-                oneCount++;
-            }
-        }
-        if (oneCount != n - 1) return -1;
-        
-        return ans;
+        return start;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        vector<vector<int> > M( n , vector<int> (n, 0));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                cin>>M[i][j];
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
             }
         }
         Solution ob;
-        cout<<ob.celebrity(M,n)<<endl;
-
+        cout << ob.celebrity(M) << endl;
     }
 }
 
