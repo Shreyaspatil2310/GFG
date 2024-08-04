33345012796fb504cfb3e05c989fad6cc0ddd41f
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-08-04 18:55:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int smallestSubWithSum(int x, vector<int>& arr) {
+        // Your code goes here
+        int i=0,j=0,sum=0,ans=INT_MAX,n=arr.size();
+        while(j<n){
+            sum+=arr[j];
+            while(sum>x){
+                ans=min(ans,j-i+1);
+                sum-=arr[i];
+                i++;
+            }
+            j++;
+        }
+        return ans==INT_MAX?0:ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int x;
+
+        cin >> x;
+        cin.ignore(); // Ignore the newline character after x
+
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        cout << obj.smallestSubWithSum(x, arr) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
