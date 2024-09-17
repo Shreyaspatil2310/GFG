08dec153729fb765d2089516901b3f85f7301353
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-09-17 11:52:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -4,13 +4,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function template for C++
+
 class Solution {
   public:
-    int getMinDiff(int arr[], int n, int k) {
-        // code here
-        sort(arr, arr+n);
+    int getMinDiff(vector<int> &arr, int k) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
         int ans = arr[n-1]-arr[0];
         int lowest = arr[0]+k, largest = arr[n-1]-k;
         for(int i=0;i<n-1;i++)
@@ -27,20 +29,29 @@ class Solution {
     }
 };
 
+
+
+
+
 //{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
         int n, k;
         cin >> k;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
+        cin.ignore();
+        vector<int> a, b, c, d;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
         Solution ob;
-        auto ans = ob.getMinDiff(arr, n, k);
+        auto ans = ob.getMinDiff(a, k);
         cout << ans << "\n";
     }
     return 0;
