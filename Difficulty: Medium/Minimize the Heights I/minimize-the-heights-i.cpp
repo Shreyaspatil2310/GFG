d9d339e0bd2462a2c09ce301f0dfbf3703c92fda
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-11-23 18:04:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
@@ -1,46 +1,54 @@
 //{ Driver Code Starts
-//Initial template for C++
-
+// Initial template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
 
-class Solution{   
-public:
-    int getMinDiff(int arr[], int n, int k) {
-        // code here
-        sort(arr,arr+n);
-        
-        int ans=arr[n-1]-arr[0];
-        
-        for(int i=1;i<n;i++)
-        {
-            int MIN= min(arr[0]+k,arr[i]-k);
-            int MAX= max(arr[i-1]+k,arr[n-1]-k);
-            ans=min(ans,MAX-MIN);
-            
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int k, vector<int> &arr) {
+        int n=arr.size();
+        sort(arr.begin() , arr.end()) ; 
+        int ans = arr[n-1] - arr[0] ; 
+        for(int i=1;i<n;i++){
+            int mini = min(arr[0]+k , arr[i]-k) ; 
+            int maxi = max(arr[i-1]+k , arr[n-1]-k) ; 
+            ans = min(ans , maxi - mini) ; 
         }
-        return ans;
-        
+        return ans; 
     }
 };
 
 //{ Driver Code Starts.
 int main() {
     int t;
+
     cin >> t;
+    cin.ignore();
+
     while (t--) {
-        int n, k;
+        int k;
         cin >> k;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
+        int n = arr.size();
         Solution ob;
-        auto ans = ob.getMinDiff(arr, n, k);
-        cout << ans << "\n";
+        int res = ob.getMinDiff(k, arr);
+        cout << res;
+
+        cout << "\n";
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
