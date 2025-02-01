8d7167e744b635737221ab21e266e8ab29894f3d
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2025-02-01 19:13:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<long long> factorialNumbers(long long n, long long x=1,vector<long long> ans={},long long a = 1) {
+        if(x>n)
+        return ans;
+        else
+        {
+        ans.emplace_back(x);
+        return factorialNumbers(n,x*(a+1),ans,a+1);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for (auto num : ans) {
+            cout << num << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
